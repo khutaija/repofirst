f7e82e2734e6bf72b536c5984a3c93a76a30cd81
khutaija <shariff.k@husky.neu.edu>
2016-01-27 20:27:34 -0800

    Second Assignment

--- Seven.java
+++ Seven.java
@@ -15,9 +15,7 @@ class Seven
 				for(int i=2;i<n;i++)
 				{   fib[0] = 1;
 					fib[1] = 1;
-					fib[i] = fib1 + fib2;
-					fib1 = fib2;
-					fib2 = fib[i];
+					fib[i] = fib[i-1] + fib[i-2];
 				}
 			}
 		
