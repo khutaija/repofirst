21031061631ae1211f80af6804e72eeb3c81f932
khutaija <shariff.k@husky.neu.edu>
2016-02-11 17:39:45 -0800

    Assignment four

---
+++ Assignment4/LastWord.java
@@ -0,0 +1,38 @@
+/**
+     * Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+     * If the last word does not exist, return 0.
+     * Note: A word is defined as a character sequence consists of non-space characters only.
+     */
+
+
+class LastWord{
+  
+    public int lengthOfLastWord(String str)
+
+    {
+      if(str.equals(""))
+      {
+        System.out.println("Error: You passed an empty string");
+          return 0; 
+      
+      }
+       else
+       { 
+       int length = 0;
+         String lastword = str.substring(str.lastIndexOf(" ")+1);
+         length = lastword.length();
+         return length;
+      
+       }
+    }
+  
+ 
+    public static void main(String[] args)
+    {
+      LastWord call = new LastWord();
+      String str1 = "Your best teacher is your last mistake";
+      int length = 0;
+      length = call.lengthOfLastWord(str1);
+      System.out.println("The length of last word is : " + length);
+    }
+  }
---
+++ Assignment4/LastWordTest.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LastWordTest {
+
+	@Test
+	public void testLengthOfLastWord() {
+		LastWord test = new LastWord();
+		int expectedOutput = 7;
+	   
+		int actualOutput = test.lengthOfLastWord("Your best teacher is your last mistake");
+	    assertEquals(expectedOutput, actualOutput);
+	    
+	    int expectedOutput1 = 0;
+	    int actualOutput1 = test.lengthOfLastWord("Your best teacher is your last mistake ");
+	    assertEquals(expectedOutput1, actualOutput1);
+	    
+	    int expectedOutput2 = 0;
+	    int actualOutput2 = test.lengthOfLastWord("");
+	    assertEquals(expectedOutput2, actualOutput2);
+	}
+
+}
