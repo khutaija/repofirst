1cf17f1dbbeb7b06ece87bd05464851e13c4168f
khutaija <shariff.k@husky.neu.edu>
2016-02-13 12:18:58 -0800

    Assignment 4

---
+++ Assignment4/CapitalizeLetters.java
@@ -0,0 +1,31 @@
+/**
+ * Capitalize the first letters of words in a String. Do not use 'split'
+ * function. e.g. "I like soccer" --> "I Like Soccer".
+ */
+
+public class CapitalizeLetters {
+
+	public String capitalizeLetters(String str) {
+		if (str.equals(""))
+			return null;
+
+		char[] arrayofstring = str.toCharArray();
+		for (int i = 0; i < arrayofstring.length; i++) {
+			if (arrayofstring[i] == ' ' && arrayofstring[i + 1] != ' ')
+				arrayofstring[i + 1] = Character.toUpperCase(arrayofstring[i + 1]);
+		}
+		String str1 = new String(arrayofstring);
+		return str1;
+	}
+
+	public static void main(String[] args) {
+
+		CapitalizeLetters call = new CapitalizeLetters();
+		String inputString = "A corporeal patronus";
+		String outputString = call.capitalizeLetters(inputString);
+		System.out.println("Input string: " + inputString);
+		System.out.println("Output string: " + outputString);
+
+	}
+
+}
---
+++ Assignment4/CapitalizeLettersTest.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CapitalizeLettersTets {
+
+	CapitalizeLetters test = new CapitalizeLetters();
+
+	@Test
+	public void testCapitalizeLetters() {
+
+		String actualOutput = test.capitalizeLetters("A corporeal patronus");
+		String expectedOutput = "A Corporeal Patronus";
+		assertEquals(expectedOutput, actualOutput);
+
+		String actualOutput1 = test.capitalizeLetters("");
+		String expectedOutput1 = null;
+		assertEquals(expectedOutput1, actualOutput1);
+
+	}
+
+}
