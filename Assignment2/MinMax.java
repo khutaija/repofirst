c674d10eee930ee25f7e055946070e42be6debf3
khutaija <shariff.k@husky.neu.edu>
2016-02-15 20:22:30 -0800

    Update MinMax.java

--- Assignment2/MinMax.java
+++ Assignment2/MinMax.java
@@ -3,25 +3,30 @@
  */
 public class MinMax {
 	public int[] findMinMax(int[] nums) {
-		int temp;
-		for (int j = 0; j < nums.length - 1; j++) {
-			for (int i = 0; i < nums.length - 1; i++) {
-				if (nums[i] > nums[i + 1]) {
-					temp = nums[i];
-					nums[i] = nums[i + 1];
-					nums[i + 1] = temp;
-				}
-			}
+		
+		if(nums==null||nums.length==0) return null;
+		
+		int[] result = new int[2];
+		result[0] = nums[0];
+		result[1] = nums[0];
+	       
+		for(int i=1; i< nums.length; i++){
+	        	if(result[0] < nums[i]){
+	        		result[0] = nums[i];
+	        	}
+	        	if(result[1] > nums[i]){
+	        		result[1] = nums[i];
+	        	}
 		}
-		return nums;
+		return result;
 	}
 
 	public static void main(String[] args) {
 		MinMax minmax = new MinMax();
 		int[] numbers = { 6, 34, 98, 13, 3, 59, 72, 23, 75 };
 		int[] var = minmax.findMinMax(numbers);
-		System.out.println("The minimum number is: " + var[0]);
-		System.out.println("The maximum number is: " + var[var.length - 1]);
+		System.out.println("The maximum number is: " + var[0]);
+		System.out.println("The minimum number is: " + var[1]);
 
 	}
 
