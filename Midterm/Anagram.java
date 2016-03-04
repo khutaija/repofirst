5215a6cc39e30caac5bc3297a77cdc7e696181d1
khutaija <shariff.k@husky.neu.edu>
2016-03-03 20:10:19 -0800

    MidTerm Exam

---
+++ Midterm/Anagram.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class Anagram {
+
+	public boolean isAnagram(String s, String t) {
+		char stringArray1[] = s.toCharArray();
+		char stringArray2[] = t.toCharArray();
+		Arrays.sort(stringArray1);
+		Arrays.sort(stringArray2);
+		String sortedS = new String(stringArray1);
+		String sortedT = new String(stringArray2);
+		if (sortedS.equals(sortedT))
+			return true;
+		return false;
+	}
+
+	public static void main(String[] args) {
+		String str1 = "anagram";
+		String str2 = "nagaram";
+		Anagram call = new Anagram();
+		boolean result = call.isAnagram(str1, str2);
+		System.out.println("The two strings are anagrams : " +result);
+
+	}
+
+}
